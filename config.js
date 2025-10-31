82c6c72d21c9582fa6f01935d6958145b5ea9488
mihin2000 <mihinshehantha@gmail.com>
2025-10-31 13:16:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/mihin2000/All-For-One/blob/main/Images/images.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 All For One Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
